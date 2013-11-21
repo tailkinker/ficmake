34b4d087b707a46e0cb0eef3c3133cdebf0c7ec8
Timothy Groves <the.tail.kinker@gmail.com>
2013-11-21 08:57:04 -0500

    More on gprofile.pas

--- gprofile.pas
+++ gprofile.pas
@@ -50,14 +50,15 @@ type
   tBaseProfile = class (tObject)
     private
       t_name : string;
-      ProfileType : byte;
       t_output : string;
       t_preconv : boolean;
+      t_separator : string;
     public
+      class function ProfileType : integer; virtual;
       property Name : string read t_name write t_name;
       property OutputDir : string read t_output write t_output;
       property UsePreconv : boolean read t_preconv write t_preconv;
-      constructor Create;
+      property Separator : string read t_separator write t_separator;
   end;
 
   tPDFProfile = class (tBaseProfile)
@@ -69,46 +70,106 @@ type
       t_landscape : boolean;
       t_columns : byte;
       t_margins : array [0..3] of longint;
-      t_separator : string;
       t_H1Mode : byte;
       t_headers : array [0..3] of tGroffHeader;
       t_OneColumnTitlePage : boolean;
+      t_flags : array [0..3] of boolean;
       procedure SetMargin (index : integer; value : longint);
       function GetMargin (index : integer) : longint;
       procedure SetHeader (index : integer; header : tGroffHeader);
       function GetHeader (index : integer) : tGroffHeader;
+      procedure SetFlag (index : integer; aflag : boolean);
+      function GetFlag (index : integer) : boolean;
     public
+      class function ProfileType : integer; override;
       property GroffFonts : tGroffFontArray read t_fonts write t_fonts;
       property PageSize : byte read t_pagesize write t_pagesize;
       property PageH : longint read t_pageh write t_pageh;
       property PageV : longint read t_pagev write t_pagev;
-      property Landscape : boolean read t_landscape write t_landscape;
       property Columns : byte read t_columns write t_columns;
       property OuterMargin  : longint index 0 read GetMargin write SetMargin;
       property InnerMargin  : longint index 1 read GetMargin write SetMargin;
       property TopMargin    : longint index 2 read GetMargin write SetMargin;
       property BottomMargin : longint index 3 read GetMargin write SetMargin;
-      property Separator : string read t_separator write t_separator;
       property H1Mode : byte read t_H1Mode write t_H1Mode;
       property OddHeader  : tGroffHeader index 0 read GetHeader write SetHeader;
       property EvenHeader : tGroffHeader index 1 read GetHeader write SetHeader;
       property OddFooter  : tGroffHeader index 2 read GetHeader write SetHeader;
       property EvenFooter : tGroffHeader index 3 read GetHeader write SetHeader;
-      property OneColumnTitlePage : boolean;
+      property OneColumnTitlePage : boolean index 0 read SetFlag write GetFlag;
+      property Landscape          : boolean index 1 read SetFlag write GetFlag;
+      property UseTBL             : boolean index 2 read GetFlag write SetFlag;
+      property UseEQN             : boolean index 3 read GetFlag write SetFlag;
   end;
+
+  tHTMLProfile = class (tBaseProfile)
+    private
+      t_flags : array [0..5] of boolean;
+      t_strings : array [0..1] of string;
+      procedure SetFlag (index : integer; aflag : boolean);
+      function GetFlag (index : integer) : boolean;
+      procedure SetString (index : integer; astring : string);
+      function GetString (index : integer) : string;
+    public
+      class function ProfileType : integer; override;
+      property BulkHTML            : boolean index 0 read GetFlag write SetFlag;
+      property DisclaimerInIndex   : boolean index 1 read GetFlag write SetFlag;
+      property DisclaimerInChapter : boolean index 2 read GetFlag write SetFlag;
+      property BookFilesInIndex    : boolean index 3 read GetFlag write SetFlag;
+      property SingleLineBooks     : boolean index 4 read GetFlag write SetFlag;
+      property SeparateBookFiles   : boolean index 5 read GetFlag write SetFlag;
+      property BackLink     : string index 0 read GetString write SetString;
+      property BackLinkText : string index 1 read GetString write SetString;
+  end;
+
+  tTextProfile = class (tBaseProfile)
+    private
+      t_flags : array [0..2] of boolean;
+      procedure SetFlag (index : integer; aflag : boolean);
+      function GetFlag (index : integer) : boolean;
+    public
+      class function ProfileType : integer; override;
+      property BulkText      : boolean index 0 read GetFlag write SetFlag;
+      property FFMLCompliant : boolean index 1 read GetFlag write SetFlag;
+      property UseTBL        : boolean index 2 read GetFlag write SetFlag;
+  end;
+
+  tEPubProfile = class (tBaseProfile)
+    private
+      t_blurb : boolean;
+      t_series : string;
+    public
+      class function ProfileType : integer; override;
+      property BlurbInEPub : boolean read t_blurb write t_blurb;
+      property EPubSeries : string read t_series write t_series;
+  end;
+
+const
+  ptBase = 0;
+  ptPDF = 1;
+  ptHTML = 2;
+  ptText = 3;
+  ptEPub = 4;
 
 implementation
 
 {$region tBaseProfile}
 
-constructor tBaseProfile.Create;
-begin
 
+class function tBaseProfile.ProfileType : integer;
+begin
+  ProfileType := 0;
 end;
+
 
 {$endregion}
 
 {$region tPDFProfile}
+
+class function tPDFProfile.ProfileType : integer;
+begin
+  ProfileType := ptPDF;
+end;
 
 procedure tPDFProfile.SetMargin (index : integer; value : longint);
 begin
@@ -130,6 +191,74 @@ begin
   GetHeader := t_headers [index];
 end;
 
+procedure tPDFProfile.SetFlag (index : integer; aflag : boolean);
+begin
+  t_flags [index] := aflag;
+end;
+
+function tPDFProfile.GetFlag (index : integer) : boolean;
+begin
+	GetFlag = t_flags [index];
+end;
+
+
+
+{$endregion}
+
+{$region tHTMLProfile}
+
+class function tHTMLProfile.ProfileType : integer;
+begin
+  ProfileType := ptHTML;
+end;
+
+procedure tHTMLProfile.SetFlag (index : integer; aflag : boolean);
+begin
+  t_flags [index] := aflag;
+end;
+
+function tHTMLProfile.GetFlag (index : integer) : boolean;
+begin
+	GetFlag = t_flags [index];
+end;
+
+procedure tHTMLProfile.SetString (index : integer; astring : string);
+begin
+	t_strings [index] := aString;
+end;
+
+function tHTMLProfile.GetString (index : integer) : string;
+begin
+	GetString := t_strings [index];
+end;
+
+{$endregion}
+
+{$region tTextProfile}
+
+class function tTextProfile.ProfileType : integer;
+begin
+  ProfileType := ptText;
+end;
+
+procedure tTextProfile.SetFlag (index : integer; aflag : boolean);
+begin
+  t_flags [index] := aflag;
+end;
+
+function tTextProfile.GetFlag (index : integer) : boolean;
+begin
+	GetFlag = t_flags [index];
+end;
+
+{$endregion}
+
+{$region tEPubProfile}
+
+class function tEPubProfile.ProfileType : integer;
+begin
+  ProfileType := ptEPub;
+end;
 
 {$endregion}
 end.
