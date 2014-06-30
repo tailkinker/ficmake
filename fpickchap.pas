0678bcd6570427630e3b4e989132fd40aca1d644
Timothy Groves <the.tail.kinker@gmail.com>
2014-06-30 09:24:24 -0400

    Added "Open File In Editor"

---
+++ fpickchap.pas
@@ -0,0 +1,82 @@
+unit fpickchap;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  gchapter;
+
+type
+
+  { TfrmPickChapter }
+
+  TfrmPickChapter = class(TForm)
+    btnOK: TButton;
+    btnCancel: TButton;
+    lstChapters: TListBox;
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+    procedure lstChaptersClick(Sender: TObject);
+    procedure lstChaptersDblClick(Sender: TObject);
+  private
+    { private declarations }
+    t_basedir : string;
+    t_chapterpick : string;
+    Chapters : tChapterList;
+    procedure SetBaseDir (aDir : string);
+  public
+    { public declarations }
+    property BaseDir : string read t_basedir write SetBaseDir;
+    property Filename : string read t_chapterpick;
+  end;
+
+implementation
+
+{$R *.lfm}
+
+procedure TfrmPickChapter.FormCreate(Sender: TObject);
+begin
+  Chapters := tChapterList.Create;
+end;
+
+procedure TfrmPickChapter.FormDestroy(Sender: TObject);
+begin
+  Chapters.Destroy;
+end;
+
+procedure TfrmPickChapter.lstChaptersClick(Sender: TObject);
+begin
+  if (lstChapters.ItemIndex > 0) then
+    if (lstChapters.ItemIndex < (Chapters.Count)) then begin
+      Chapters.SelectAt (lstChapters.ItemIndex);
+      t_chapterpick := Chapters.Current.Filename;
+    end;
+end;
+
+procedure TfrmPickChapter.lstChaptersDblClick(Sender: TObject);
+begin
+  if (lstChapters.ItemIndex > 0) then
+    if (lstChapters.ItemIndex < (Chapters.Count)) then begin
+      Chapters.SelectAt (lstChapters.ItemIndex);
+      t_chapterpick := Chapters.Current.Filename;
+    end;
+  ModalResult := mrOk;
+end;
+
+procedure TfrmPickChapter.SetBaseDir (aDir : string);
+var
+  index : integer;
+begin
+  t_basedir := aDir;
+  Chapters.BaseDir := aDir;
+  Chapters.Load;
+
+  lstChapters.Items.Clear;
+  for index := 0 to (Chapters.Count - 1) do
+    lstChapters.Items.Add (Chapters.Name (index));
+end;
+
+end.
+
